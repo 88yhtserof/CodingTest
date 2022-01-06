6884415618d3ef4877fcfb6d35ecc2dfaef192a3
LimYunhwi <88yhtserof@naver.com>
2022-01-06 23:32:50 +0900

    FunctionDevelopment (2)
    1~11 테스트 케이스 모두 통과

--- FunctionDevelopment.playground/Contents.swift
+++ FunctionDevelopment.playground/Contents.swift
@@ -2,13 +2,16 @@ import Foundation
 
 func solution(_ progresses:[Int], _ speeds:[Int]) -> [Int] {
     var results = progresses
+    var resultsSpeed = speeds
+    
     var releases: [Int] = [] //배포한 기능의 개수를 저장할 배열
+    var returnValue: Int
     
     while true {//하루를 의미하는 반복문
-        var returnValue = 0
+        returnValue = 0
         
         for n in 0..<results.count { //하루동안 개발함을 의미
-            results[n] += speeds[n]
+            results[n] += resultsSpeed[n]
         }
         
         for n in 0..<results.count {//배포 가능여부 확인
@@ -25,6 +28,7 @@ func solution(_ progresses:[Int], _ speeds:[Int]) -> [Int] {
             
             //pop : 배포 완료한 기능을 작업 진도란에서 제거
             results.removeSubrange(0..<returnValue)
+            resultsSpeed.removeSubrange(0..<returnValue)
         }
         
         if results.isEmpty { //더 이상 배포할 기능이 없다면 기능 작업 종료
@@ -34,3 +38,5 @@ func solution(_ progresses:[Int], _ speeds:[Int]) -> [Int] {
     
     return releases
 }
+
+solution([40,93,30,55,60,65], [60,1,30,5,10,7])
