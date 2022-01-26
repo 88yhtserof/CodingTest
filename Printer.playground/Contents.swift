ac09e15c16ef761b0a7edb33c61929172632cc20
LimYunhwi <88yhtserof@naver.com>
2022-01-27 00:45:19 +0900

    Printer 프린터 문제 설명 주석 추가

--- Printer.playground/Contents.swift
+++ Printer.playground/Contents.swift
@@ -6,6 +6,7 @@
  2. 나머지 인쇄 대기목록에서 J보다 중요도가 높은 문서가 한 개라도 존재하면 J를 대기목록의 가장 마지막에 넣습니다.
  3. 그렇지 않으면 J를 인쇄합니다.
  */
+//일부 테스트케이스만 통과. 반례를 찾지 못해 풀이 중지
 import Foundation
 
 func solution(_ priorities:[Int], _ location:Int) -> Int {
